1eef64db32421bb619522552fbf89b39fa6cb2d3
Oded Niv <oded.niv@gmail.com>
2024-11-16 14:48:37 +0000

    config and robots.txt

---
+++ src/data/site.config.ts
@@ -0,0 +1,21 @@
+interface SiteConfig {
+	site: string
+	author: string
+	title: string
+	description: string
+	lang: string
+	ogLocale: string
+	shareMessage: string
+	paginationSize: number
+}
+
+export const siteConfig: SiteConfig = {
+	site: 'https://odedniv.me/', // Write here your website url
+	author: 'Oded Niv', // Site author
+	title: 'Oded Niv', // Site title.
+	description: 'Welcome to my website!', // Description to display in the meta tags
+	lang: 'en-GB',
+	ogLocale: 'en_GB',
+	shareMessage: 'Share this post', // Message to share a post on social media
+	paginationSize: 6 // Number of posts per page
+}
