99552100f7fb76b7b0dfeefc6b53b93cfddd07b4
Oded Niv <oded.niv@gmail.com>
2024-11-16 14:48:37 +0000

    config and robots.txt

---
+++ src/data/categories.ts
@@ -0,0 +1,5 @@
+// List of categories for blog posts
+export const CATEGORIES = [
+	'Software Engineering',
+	'Shower Thoughts',
+] as const
---
+++ src/data/links.ts
@@ -0,0 +1,18 @@
+import LinkedinIcon from '@/components/icons/LinkedinIcon'
+import GithubIcon from '@/components/icons/GithubIcon'
+
+// ADD YOUR SOCIAL NETWORKS HERE
+export const SOCIALNETWORKS = [
+	{
+		name: 'LinkedIn',
+		url: 'https://linkedin.com/in/odedniv',
+		icon: LinkedinIcon
+
+	},
+
+	{
+		name: 'GitHub',
+		url: 'https://github.com/odedniv',
+		icon: GithubIcon
+	}
+] as const
