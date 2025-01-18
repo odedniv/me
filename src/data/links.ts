32f1b16481081a27f6e69867c44cd464ce9447d9
Oded Niv <oded.niv@gmail.com>
2025-01-18 17:03:22 +0000

    resume link

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,15 +1,19 @@
+import ResumeIcon from '@/components/icons/ResumeIcon'
 import LinkedinIcon from '@/components/icons/LinkedinIcon'
 import GithubIcon from '@/components/icons/GithubIcon'
 
 // ADD YOUR SOCIAL NETWORKS HERE
 export const SOCIALNETWORKS = [
 	{
+		name: 'Resume',
+		url: 'https://resume.odedniv.me',
+		icon: ResumeIcon
+	},
+	{
 		name: 'LinkedIn',
 		url: 'https://linkedin.com/in/odedniv',
 		icon: LinkedinIcon
-
 	},
-
 	{
 		name: 'GitHub',
 		url: 'https://github.com/odedniv',
