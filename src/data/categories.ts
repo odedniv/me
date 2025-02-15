415af68d3e7713db4eca525817d6d33390dc16c9
Oded Niv <oded.niv@gmail.com>
2025-02-15 15:22:29 +0000

    explicit category slugs

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,5 +1,6 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Software Engineering',
-	'Shower Thoughts',
+	{ slug: 'software', title: 'Software Engineering' },
+	{ slug: 'shower', title: 'Shower Thoughts' },
 ] as const
+// The keys must equal the slug fields.
